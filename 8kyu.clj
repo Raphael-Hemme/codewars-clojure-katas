21265c9843358b0067117d12903a224e638dd093
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2023-06-07 08:30:05 +0200

    Add kata #2

--- 8kyu.clj
+++ 8kyu.clj
@@ -5,3 +5,8 @@
                 (< d 3) 0
                 (< d 7) 20
                 (>= d 7) 50)))
+
+;; Grasshopper - Messi goals function
+;; https://www.codewars.com/kata/55f73be6e12baaa5900000d4/train/clojure
+(defn goals [la-liga-goals copa-del-rey-goals champions-league-goals]
+  (+ la-liga-goals copa-del-rey-goals champions-league-goals))
