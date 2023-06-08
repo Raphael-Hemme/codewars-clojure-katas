a30db79a2f922a08206123e7e1d1aa4bdf18c205
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2023-06-08 09:16:59 +0200

    Put solutions into rich comment forms to be able to more easily evaluate them in the repl

--- 8kyu.clj
+++ 8kyu.clj
@@ -1,22 +1,44 @@
-;; Transportation on Vacation
-;; https://www.codewars.com/kata/568d0dd208ee69389d000016/train/clojure
-(defn calc-rental-car-cost [d]
-  (- (* d 40) (cond
-                (< d 3) 0
-                (< d 7) 20
-                (>= d 7) 50)))
+(comment
+  ;; Transportation on Vacation
+  ;; https://www.codewars.com/kata/568d0dd208ee69389d000016/train/clojure
+  (defn calc-rental-car-cost [d]
+    (- (* d 40) (cond
+                  (< d 3) 0
+                  (< d 7) 20
+                  :else 50)))
 
-;; Grasshopper - Messi goals function
-;; https://www.codewars.com/kata/55f73be6e12baaa5900000d4/train/clojure
-(defn goals [la-liga-goals copa-del-rey-goals champions-league-goals]
-  (+ la-liga-goals copa-del-rey-goals champions-league-goals))
+  (calc-rental-car-cost 6)
+  
+  :rcf)
 
-;; Reduce but Grow
-;; https://www.codewars.com/kata/57f780909f7e8e3183000078/train/clojure
-(defn reduce-multiply [xs]
-  (reduce * xs))
+(comment 
+  ;; Grasshopper - Messi goals function
+  ;; https://www.codewars.com/kata/55f73be6e12baaa5900000d4/train/clojure
+  (defn goals [la-liga-goals copa-del-rey-goals champions-league-goals]
+    (+ la-liga-goals copa-del-rey-goals champions-league-goals))
+  
+  (goals 5 18 3)
+  
+  :rcf)
+  
 
-;; Grasshopper - Summation
-;; https://www.codewars.com/kata/55d24f55d7dd296eb9000030/train/clojure
-(defn summation [n]
-  (apply + (range 1 (inc n))))
+(comment 
+  ;; Reduce but Grow
+  ;; https://www.codewars.com/kata/57f780909f7e8e3183000078/train/clojure
+  (defn reduce-multiply [xs]
+    (reduce * xs))
+  
+  (reduce-multiply (vector 3 4 5))
+  
+  :rcf)
+  
+
+(comment 
+  ;; Grasshopper - Summation
+  ;; https://www.codewars.com/kata/55d24f55d7dd296eb9000030/train/clojure
+  (defn summation [n]
+    (apply + (range 1 (inc n))))
+  
+  (summation 42)
+
+  :rcf)
