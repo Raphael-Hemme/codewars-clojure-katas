41c0bb8c6048fd2b9e2a97e6669308c66ddacac3
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2023-06-08 09:53:48 +0200

    Add kata #5

--- 8kyu.clj
+++ 8kyu.clj
@@ -8,37 +8,55 @@
                   :else 50)))
 
   (calc-rental-car-cost 6)
-  
-  :rcf)
 
-(comment 
+
+
   ;; Grasshopper - Messi goals function
   ;; https://www.codewars.com/kata/55f73be6e12baaa5900000d4/train/clojure
   (defn goals [la-liga-goals copa-del-rey-goals champions-league-goals]
     (+ la-liga-goals copa-del-rey-goals champions-league-goals))
-  
-  (goals 5 18 3)
-  
-  :rcf)
-  
 
-(comment 
+  (goals 5 18 3)
+
+
+
   ;; Reduce but Grow
   ;; https://www.codewars.com/kata/57f780909f7e8e3183000078/train/clojure
   (defn reduce-multiply [xs]
     (reduce * xs))
-  
-  (reduce-multiply (vector 3 4 5))
-  
-  :rcf)
-  
 
-(comment 
+  (reduce-multiply (vector 3 4 5))
+
+
+
   ;; Grasshopper - Summation
   ;; https://www.codewars.com/kata/55d24f55d7dd296eb9000030/train/clojure
   (defn summation [n]
     (apply + (range 1 (inc n))))
-  
+
   (summation 42)
 
+
+  
+  ;; Expressions matter
+  ;; https://www.codewars.com/kata/5ae62fcf252e66d44d00008e/train/clojure
+  (defn expression-matter [a b c]
+    (let [ab-m (* a b)
+          bc-m (* b c)
+          ab-a (+ a b)
+          bc-a (+ b c)]
+      ;; (println ab-m bc-m ab-a bc-a)
+     (max 
+      (* a b c)
+      (+ a b c) 
+      (* ab-a c)
+      (* a bc-a) 
+      (+ ab-m c) 
+      (+ a bc-m))
+      ))
+  
+  (expression-matter 5 1 3)
+
+
+  
   :rcf)
